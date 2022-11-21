80ce173a002da7d52c3eca9c77839b95a532de83
xuu0411 <jie350182@gmail.com>
2022-11-21 20:14:45 +0800

    新增分數計算

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -8,12 +8,14 @@ public class GameManager : MonoBehaviour
     GameObject car;              // 用來儲存汽車物件
     GameObject flag;             // 用來儲存旗子物件
     GameObject distance;         // 用來儲存UI文字物件
+    GameObject score;         // 用來儲存分數文字物件
 
     void Start() // 將場景中的物件放到已宣告的各個物件中
     {
         car = GameObject.Find("car");         
         flag = GameObject.Find("flag");
         distance = GameObject.Find("Distance");
+        score = GameObject.Find("Score");
     }
 
     void Update()
@@ -22,5 +24,6 @@ public class GameManager : MonoBehaviour
         float length = flag.transform.position.x - car.transform.position.x;
         // 將旗子與汽車的距離顯示在UI上面
         distance.GetComponent<Text>().text = "距離目標還有 " + length.ToString("F2") + "m";
+        score.GetComponent<Text>().text = "得分 " + length.ToString("F2") + "m";
     }
 }
